a219f752e5c1aec120125ef20fa2d4fc330ba60c
d1av <davi4alves@gmail.com>
2022-12-23 19:17:48 -0300

    feat: add average in native sql

--- uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
+++ uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
@@ -1,9 +1,26 @@
 package com.devsuperior.uri2737.repositories;
 
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2737.entities.Lawyer;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LawyerRepository extends JpaRepository<Lawyer, Long> {
+    @Query(nativeQuery = true, value = "(select name, customers_number AS customersNumber " +
+            "from lawyers l " +
+            "order by customers_number desc " +
+            "limit 1) " +
+            "union all " +
+            "(select name, customers_number " +
+            "from lawyers l " +
+            "order by customers_number asc " +
+            "limit 1) " +
+            "union all " +
+            "(select 'Average', round(avg(customers_number), 0) " +
+            "from lawyers l)")
+    List<LawyerMinProjection> search1();
 
 }
