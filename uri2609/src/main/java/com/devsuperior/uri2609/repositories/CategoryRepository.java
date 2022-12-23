ed0c106b8d1053fc286cc02b9f74bee39de3d7b4
d1av <davi4alves@gmail.com>
2022-12-23 19:00:20 -0300

    feat: add join and sum in JPQL

--- uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
@@ -1,9 +1,25 @@
 package com.devsuperior.uri2609.repositories;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2609.entities.Category;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @Query(nativeQuery = true, value = "select c.name, SUM(p.amount) " +
+            "from categories c " +
+            "inner join products p on c.id = p.id_categories " +
+            "group by c.name")
+    List<CategorySumProjection> search1();
+
+    @Query("select new com.devsuperior.uri2609.dto.CategorySumDTO(obj.category.name, SUM(obj.amount)) " +
+            "from Product obj " +
+            "group by obj.category.name")
+    List<CategorySumDTO> search2();
 
 }
